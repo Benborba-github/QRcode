6ecfcdeea4f783d9b3a1e1dc9e3d2517618ffc0b
qiaojiazou <qiaojiazhou_vendor@sensetime.com>
2021-02-06 11:01:12 +0800

    linktoqrcode

--- js/background.js
+++ js/background.js
@@ -1,9 +1,13 @@
 
 chrome.contextMenus.create({
-    title: '生成二维码：%s', // %s表示选中的文字
-    contexts: ['selection'], // 只有当选中文字时才会出现此右键菜单
+    title: '生成二维码', // %s表示选中的文字
+    contexts: ['selection',"link","editable"], // 只有当选中文字时才会出现此右键菜单
     onclick: function(val){
-        chrome.tabs.create({url: "html/index.html?selectionText=" + val.selectionText });
+        if (val.linkUrl) {
+            chrome.tabs.create({url: "html/index.html?selectionText=" + val.linkUrl });
+        }else if (val.selectionText) {
+            chrome.tabs.create({url: "html/index.html?selectionText=" + val.selectionText });
+        }
     }
 });
 
